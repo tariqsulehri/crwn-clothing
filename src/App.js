ff071c3c1da15dbe13672e73aa80fae12fde9ad5
tariqsulehri <tariq_sulehri@hotmail.com>
2021-04-15 18:21:00 +0500

    Shop Page, colletion Page alonwith collection Item and Styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
-
-
-const HatsPage = () => (
-  <div>
-    <h1>  HATS PAGE </h1>
-  </div>
-)
-
+import ShopPage from './pages/shop/shop.component';
+import './App.css';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
